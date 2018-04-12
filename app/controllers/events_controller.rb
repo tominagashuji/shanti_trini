8d502a37c9e7f192511890882730511a54c65646
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-04-12 13:42:04 +0900

    mail

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,6 +23,7 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
+        EventMailer.event_mail(@event).deliver
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
         format.json { render :show, status: :created, location: @event }
       else
