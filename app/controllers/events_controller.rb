09cf83dc8ba314ac64292eb156e068cd6fb3a9ca
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-04-20 19:17:26 +0900

    本番２

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -46,7 +46,7 @@ class EventsController < ApplicationController
     respond_to do |format|
       if @event.save
         EventMailer.event_mail(@event).deliver
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to events_path, notice: 'イベント作成に成功しました！' }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
@@ -58,7 +58,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.html { redirect_to @event, notice: 'イベント更新に成功しました！' }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
@@ -70,7 +70,7 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.html { redirect_to events_url, notice: 'イベントを削除しました。' }
       format.json { head :no_content }
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to @user, notice: 'ユーザー情報の登録が完了しました！' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'ユーザー情報を更新しました！' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'ユーザー情報を削除しました！' }
       format.json { head :no_content }
     end
   end
