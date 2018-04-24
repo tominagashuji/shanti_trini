6ae3b2b9d79b4df288e9b518a9fb2af3bf3dd799
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-04-24 19:47:46 +0900

    リファクタリング終了リリース

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do |n|
+100.times do |n|
   name = Faker::Name.unique.name
   email = Faker::Internet.unique.email
   password = "password"
-  # image = Faker::Avatar.image
   random = Random.new
+  password = "password"
 
   user = User.new(name: name,
                email: email,
